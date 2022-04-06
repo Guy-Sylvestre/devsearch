5abc44319648c7145557acd21a10a1b54c3fa82d
Danho Guy Sylvestre KRAKOU <guysylvestre876@gmail.com>
2022-04-06 19:25:41 +0000

    02 The Basics ====== 001 Views & URL's

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request)-> str:
+    return HttpResponse("Here are our products")
+
+def project(request, pk) -> str:
+    return HttpResponse("Single project with key is " + str(pk))
