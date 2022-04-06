411e9e0734dc87994b04adfd0bc70478c9fd2ecb
Danho Guy Sylvestre KRAKOU <guysylvestre876@gmail.com>
2022-04-06 19:07:01 +0000

    01 Introduction ====== FINIIR

--- devsearch/devsearch/asgi.py
+++ devsearch/devsearch/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for devsarch project.
+ASGI config for devsearch project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsarch.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsearch.settings')
 
 application = get_asgi_application()
