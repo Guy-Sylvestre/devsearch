398c616fdae57b177daf05feccb118b131960b47
Danho Guy Sylvestre KRAKOU <guysylvestre876@gmail.com>
2022-04-06 19:48:06 +0000

    02 The Basics ====== 002 Templates & Template Inheritance

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def projects(request)-> str:
-    return HttpResponse("Here are our products")
+    return render(request, 'projects/projects.html')
 
 def project(request, pk) -> str:
-    return HttpResponse("Single project with key is " + str(pk))
+    return render(request, 'projects/single-project.html')
