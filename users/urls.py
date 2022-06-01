c8734bb68929626b4c34ed430a7941a6c6697580
Danho Guy Sylvestre KRAKOU <guysylvestre876@gmail.com>
2022-06-01 18:37:55 +0000

    002 User Registration

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,11 @@ from . import views
 urlpatterns = [
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
+    path("register/", views.registerUser, name="register"),
+    
     path("", views.profiles, name="profiles"),
     path("profile/<str:pk>/", views.userProfile, name="user-profile"),
 ]
+
+
+# 3min43s
--- users/views.py
+++ users/views.py
@@ -5,13 +5,16 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 from django.shortcuts import render, redirect
 from .models import Profile
 
 
 def loginPage(request):
-
-
+    """
+        Systeme de loging
+    """
+    
     if request.user.is_authenticated:
         return redirect("profiles")
         
@@ -32,18 +35,45 @@ def loginPage(request):
         else:
             messages.error(request, "Username OR password is incorrect")
 
-    context = {
 
-    }
-
-    return render(request, "users/login_register.html", context)
+    return render(request, "users/login.html")
 
 
 def logoutUser(request):
+    """
+        Systeme de deconnexion
+    """
     logout(request)
     messages.error(request, "User was logged out")
     return redirect("login")
 
+
+def registerUser(request):
+    """
+        Creer un utilisateur
+    """
+
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, "User account was created!")
+
+            login(request, user)
+            return redirect("profiles")
+        else:
+            messages.success(request, "An error has occurred during Registration")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "users/register.html", context)
 
 
 def profiles(request):
