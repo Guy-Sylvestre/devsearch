103fc130003f21195090342884419decdcc1fa72
Danho Guy Sylvestre KRAKOU <guysylvestre876@gmail.com>
2022-06-07 19:06:24 +0000

    002 User Edit Profile

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 """
     Definir les actions notificatives lors d'un post
 """
+from cgi import print_arguments
 from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
@@ -20,9 +21,28 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+    print("------------------------------------- createProfile ------------------------------------")
     print("profile Saved!")
     print('Instance:', instance)
     print("CREATED:", created)
+    print("-------------------------------------------------------------------------")
+    
+    
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+    
+    print("------------------------------ updateUser -------------------------------------------")
+    print("profile Saved!")
+    print('Instance:', instance)
+    print("CREATED:", created)
+    print("-------------------------------------------------------------------------")
 
 
 def deleteUser(sender, instance, **kwargs):
@@ -31,7 +51,10 @@ def deleteUser(sender, instance, **kwargs):
     """
     user = instance.user
     user.delete()
-    print("Deleting user ...")
+    print("------------------------------- deleteUser ------------------------------------------")
+    print("Deleting user ... ")
+    print("-------------------------------------------------------------------------")
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 from django.shortcuts import render, redirect
 from .models import Profile
 
@@ -70,7 +70,7 @@ def registerUser(request):
             messages.success(request, "User account was created!")
 
             login(request, user)
-            return redirect("profiles")
+            return redirect("edit-account")
         else:
             messages.success(request, "An error has occurred during Registration")
 
@@ -124,3 +124,22 @@ def userAccount(request):
     }
     
     return render(request, "users/account.html", context)
+
+
+@login_required(login_url="login")
+def editAccount(request):
+    
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("account")
+    
+    context = {
+        "form": form,
+    }
+    
+    return render(request, "users/profile_form.html", context)
